b794892ec523c8955178006af8ab4b6fd070effa
ELIEZER BITTENCOURT <66735493+ELIEZERBITTENCOURT@users.noreply.github.com>
2021-03-12 16:54:13 -0300

    :package: alteração no menu hamburguer.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-document.querySelector("#menu-bar").addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-});
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
 
 document.querySelector("#qtde").addEventListener("change", atualizarPreco)
 document.querySelector("#js").addEventListener("change", atualizarPreco)
 document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
 document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
-document.querySelector("#prazo").addEventListener("change", function(){
+document.querySelector("#prazo").addEventListener("change", function () {
     const prazo = document.querySelector("#prazo").value
-    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas` 
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
     atualizarPreco()
 })
 
@@ -17,23 +16,13 @@ function atualizarPreco(){
     const qtde = document.querySelector("#qtde").value
     const temJS = document.querySelector("#js").checked
     const incluiLayout = document.querySelector("#layout-sim").checked
-    const excluiLayout = document.querySelector("#layout-nao").checked
     const prazo = document.querySelector("#prazo").value
-
-
+  
     let preco = qtde * 100;
     if (temJS) preco *= 1.1
     if (incluiLayout) preco += 500
     let taxaUrgencia = 1 - prazo*0.1;
     preco *= 1 + taxaUrgencia
-
+   
     document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
 }
-
-function onClickMenu(){
-    document.getElementById("menu").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change");
-}
-
-
